a1a784055401de47ad8fe84a68e287df4b302fd1
kambe.kenta <kenta.k199405@gmail.com>
2021-02-03 19:50:45 +0900

    added auth file type.

--- src/features/types.ts
+++ src/features/types.ts
@@ -1,4 +1,7 @@
-
+export interface File extends Blob {
+    readonly lastModified: number;
+    readonly name: string;
+  }
 export interface PROPS_AUTHEN {
     email: string;
     password: string;
