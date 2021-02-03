cf912b0c9508859f110b9be28a951b30b807a7bf
kambe.kenta <kenta.k199405@gmail.com>
2021-02-03 19:56:40 +0900

    added modules.

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,3 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+import axios from "axios";
--- src/features/types.ts
+++ src/features/types.ts
@@ -2,17 +2,18 @@ export interface File extends Blob {
     readonly lastModified: number;
     readonly name: string;
   }
-export interface PROPS_AUTHEN {
+  /*authSlice.ts*/
+  export interface PROPS_AUTHEN {
     email: string;
     password: string;
   }
-
+  
   export interface PROPS_PROFILE {
     id: number;
     nickName: string;
     img: File | null;
   }
-
+  
   export interface PROPS_NICKNAME {
     nickName: string;
   }
