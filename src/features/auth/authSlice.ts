b9f87de0c9d3af136df4144b1818e87b4ba28754
kambe.kenta <kenta.k199405@gmail.com>
2021-02-05 19:42:18 +0900

    added async login

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,72 +1,85 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 import axios from "axios";
+import { PROPS_AUTHEN, PROPS_PROFILE, PROPS_NICKNAME } from "../types";
 
 const apiUrl = process.env.REACT_APP_DEV_API_URL;
 
+export const fetchAsyncLogin = createAsyncThunk(
+  "auth/post",
+  async (authen: PROPS_AUTHEN) => {
+    const res = await axios.post(`${apiUrl}authen/jwt/create`, authen, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.data;
+  }
+);
+
 export const authSlice = createSlice({
-    name: "auth",
-    initialState: {
-      openSignIn: true,
-      openSignUp: false,
-      openProfile: false,
-      isLoadingAuth: false,
-      myprofile: {
+  name: "auth",
+  initialState: {
+    openSignIn: true,
+    openSignUp: false,
+    openProfile: false,
+    isLoadingAuth: false,
+    myprofile: {
+      id: 0,
+      nickName: "",
+      userProfile: 0,
+      created_on: "",
+      img: "",
+    },
+    profiles: [
+      {
         id: 0,
         nickName: "",
         userProfile: 0,
         created_on: "",
         img: "",
       },
-      profiles: [
-        {
-          id: 0,
-          nickName: "",
-          userProfile: 0,
-          created_on: "",
-          img: "",
-        },
-      ],
+    ],
+  },
+  reducers: {
+    fetchCredStart(state) {
+      state.isLoadingAuth = true;
     },
-    reducers: {
-      fetchCredStart(state) {
-        state.isLoadingAuth = true;
-      },
-      fetchCredEnd(state) {
-        state.isLoadingAuth = false;
-      },
-      setOpenSignIn(state) {
-        state.openSignIn = true;
-      },
-      resetOpenSignIn(state) {
-        state.openSignIn = false;
-      },
-      setOpenSignUp(state) {
-        state.openSignUp = true;
-      },
-      resetOpenSignUp(state) {
-        state.openSignUp = false;
-      },
-      setOpenProfile(state) {
-        state.openProfile = true;
-      },
-      resetOpenProfile(state) {
-        state.openProfile = false;
-      },
-      editNickname(state, action) {
-        state.myprofile.nickName = action.payload;
-      },
+    fetchCredEnd(state) {
+      state.isLoadingAuth = false;
     },
-  });
+    setOpenSignIn(state) {
+      state.openSignIn = true;
+    },
+    resetOpenSignIn(state) {
+      state.openSignIn = false;
+    },
+    setOpenSignUp(state) {
+      state.openSignUp = true;
+    },
+    resetOpenSignUp(state) {
+      state.openSignUp = false;
+    },
+    setOpenProfile(state) {
+      state.openProfile = true;
+    },
+    resetOpenProfile(state) {
+      state.openProfile = false;
+    },
+    editNickname(state, action) {
+      state.myprofile.nickName = action.payload;
+    },
+  },
+});
 
-  export const {
-    fetchCredStart,
-    fetchCredEnd,
-    setOpenSignIn,
-    resetOpenSignIn,
-    setOpenSignUp,
-    resetOpenSignUp,
-    setOpenProfile,
-    resetOpenProfile,
-    editNickname,
-  } = authSlice.actions;
+export const {
+  fetchCredStart,
+  fetchCredEnd,
+  setOpenSignIn,
+  resetOpenSignIn,
+  setOpenSignUp,
+  resetOpenSignUp,
+  setOpenProfile,
+  resetOpenProfile,
+  editNickname,
+} = authSlice.actions;
