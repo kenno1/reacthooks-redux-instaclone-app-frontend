ea65771900401edc6726ff870907a0bd6e75b59f
kambe.kenta <kenta.k199405@gmail.com>
2021-02-06 14:02:12 +0900

    added import objects.

--- src/features/post/Post.tsx
+++ src/features/post/Post.tsx
@@ -1,4 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "./Post.module.css";
+
+import { makeStyles } from "@material-ui/core/styles";
+import { Avatar, Divider, Checkbox } from "@material-ui/core";
+import { Favorite, FavoriteBorder } from "@material-ui/icons";
+
+import AvatarGroup from "@material-ui/lab/AvatarGroup";
+
+import { useSelector, useDispatch } from "react-redux";
+import { AppDispatch } from "../../app/store";
+
+import { selectProfiles } from "../auth/authSlice";
+
+import {
+  selectComments,
+  fetchPostStart,
+  fetchPostEnd,
+  fetchAsyncPostComment,
+  fetchAsyncPatchLiked,
+} from "./postSlice";
+
+import { PROPS_POST } from "../types";
 
 export const Post = () => {
     return (
