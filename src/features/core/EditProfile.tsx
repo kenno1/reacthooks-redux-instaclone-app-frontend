ba90e3149d3a97216161cfd62b4c0ac9f3438edb
kambe.kenta <kenta.k199405@gmail.com>
2021-02-07 09:47:47 +0900

    added import objects.

--- src/features/core/EditProfile.tsx
+++ src/features/core/EditProfile.tsx
@@ -1,4 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import Modal from "react-modal";
+import styles from "./Core.module.css";
+
+import { useSelector, useDispatch } from "react-redux";
+import { AppDispatch } from "../../app/store";
+
+import { File } from "../types";
+
+import {
+  editNickname,
+  selectProfile,
+  selectOpenProfile,
+  resetOpenProfile,
+  fetchCredStart,
+  fetchCredEnd,
+  fetchAsyncUpdateProf,
+} from "../auth/authSlice";
+
+import { Button, TextField, IconButton } from "@material-ui/core";
+import { MdAddAPhoto } from "react-icons/md";
+
 
 export default function EditProfile() {
     return (
