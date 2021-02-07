24fe214c08149ee1c85fa7b02b38b91d95288d28
kambe.kenta <kenta.k199405@gmail.com>
2021-02-07 09:54:34 +0900

    added import objects.

--- src/features/core/NewPost.tsx
+++ src/features/core/NewPost.tsx
@@ -1,4 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
+import Modal from "react-modal";
+import { useSelector, useDispatch } from "react-redux";
+import { AppDispatch } from "../../app/store";
+
+import styles from "./Core.module.css";
+
+import { File } from "../types";
+
+import {
+  selectOpenNewPost,
+  resetOpenNewPost,
+  fetchPostStart,
+  fetchPostEnd,
+  fetchAsyncNewPost,
+} from "../post/postSlice";
+
+import { Button, TextField, IconButton } from "@material-ui/core";
+import { MdAddAPhoto } from "react-icons/md";
 
 export default function  () {
     return (
